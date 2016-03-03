47a830a4ae21730157464aa08d0aeab834cf96fa
michael <mpalmer16@gmail.com>
2016-03-03 16:06:13 -0600

    small changes

--- src/com/mockedwards/client/MockEdwardsClient.java
+++ src/com/mockedwards/client/MockEdwardsClient.java
@@ -6,23 +6,41 @@ import com.mockedwards.factory.IntegrationFactory;
 import com.mockedwards.integration.CCIntegration;
 import com.mockedwards.integration.IntegrationType;
 import com.mockedwards.integration.OSCIntegration;
+import com.mockqueue.service.Message;
+import com.mockqueue.service.MockQueueService;
+import com.mockqueue.service.MockQueueServiceService;
 import java.util.logging.Logger;
 
 
 public class MockEdwardsClient {
     
     private static final Logger LOGGER = Logger.getLogger("MockEdwardsClient");
+    private static final MockQueueService queue = new MockQueueServiceService().getMockQueueServicePort();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException  {
         
-        MockIntegrationResponse ccResponse  = doCCIntegration(new MockIntegrationRequest(
-                IntegrationType.PayTrace, "100.00", "Authorization"));
         
-        MockIntegrationResponse oscResponse = doOSCIntegration(new MockIntegrationRequest(
-                IntegrationType.SalesCloud, "TEST OP WS", "READ"));
+        while(true) {
+            
+            Thread.sleep(4000);
+            Message message = queue.getNextMessage();
         
-        LOGGER.info(ccResponse.getResponse());
-        LOGGER.info(oscResponse.getResponse());
+            if(message.getId() != -1) {
+                
+                LOGGER.info("MESSAGE READ");
+                
+                MockIntegrationResponse ccResponse  = doCCIntegration(new MockIntegrationRequest(
+                        IntegrationType.PayTrace, "100.00", "Authorization"));
+
+        //        MockIntegrationResponse oscResponse = doOSCIntegration(new MockIntegrationRequest(
+        //                IntegrationType.SalesCloud, "TEST OP WS", "READ"));
+
+                LOGGER.info(ccResponse.getResponse());
+        //        LOGGER.info(oscResponse.getResponse());
+            } else {
+                LOGGER.info("NO MESSAGES");
+            }
+        }
     }
 
     private static MockIntegrationResponse doCCIntegration(MockIntegrationRequest mockRequest) {
