28746b60ee98bab5fabb868d89517bead864bd34
michael <mpalmer16@gmail.com>
2016-03-03 22:23:22 -0600

    routing requests as messages are written to the queue

--- src/com/mockedwards/client/MockEdwardsClient.java
+++ src/com/mockedwards/client/MockEdwardsClient.java
@@ -4,42 +4,57 @@ import com.mockedwards.dto.MockIntegrationRequest;
 import com.mockedwards.dto.MockIntegrationResponse;
 import com.mockedwards.factory.IntegrationFactory;
 import com.mockedwards.integration.CCIntegration;
-import com.mockedwards.integration.IntegrationType;
 import com.mockedwards.integration.OSCIntegration;
 import com.mockqueue.service.Message;
 import com.mockqueue.service.MockQueueService;
 import com.mockqueue.service.MockQueueServiceService;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class MockEdwardsClient {
-    
-    private static final Logger LOGGER = Logger.getLogger("MockEdwardsClient");
-    private static final MockQueueService queue = new MockQueueServiceService().getMockQueueServicePort();
 
-    public static void main(String[] args) throws InterruptedException  {
-        
-        
-        while(true) {
-            
+    private static final Logger logger = Logger.getLogger("MockEdwardsClient");
+    private static final MockQueueService queue = new MockQueueServiceService().getMockQueueServicePort();
+    
+//    use this to generate the new jar file for the queue service
+//    wsimport -d ./MockQueue -keep -clientjar mockqueue.jar http://localhost:8080/MockQueueService/MockQueueService?wsdl
+
+    public static void main(String[] args) throws InterruptedException {
+
+        while (true) {
+
             Thread.sleep(4000);
             Message message = queue.getNextMessage();
-        
-            if(message.getId() != -1) {
-                
-                LOGGER.info("MESSAGE READ");
-                
-                MockIntegrationResponse ccResponse  = doCCIntegration(new MockIntegrationRequest(
-                        IntegrationType.PayTrace, "100.00", "Authorization"));
 
-        //        MockIntegrationResponse oscResponse = doOSCIntegration(new MockIntegrationRequest(
-        //                IntegrationType.SalesCloud, "TEST OP WS", "READ"));
-
-                LOGGER.info(ccResponse.getResponse());
-        //        LOGGER.info(oscResponse.getResponse());
-            } else {
-                LOGGER.info("NO MESSAGES");
+            if (message.getId() != -1) {
+                routeMessage(message);
             }
+        }
+    }
+
+    private static void routeMessage(Message m) {
+
+        switch (m.getType()) {
+
+            case PAY_TRACE:
+//                MockIntegrationResponse ccResponse  = doCCIntegration(new MockIntegrationRequest(
+//                        IntegrationType.PayTrace, "100.00", "Authorization"));
+//                logger.info(ccResponse.getResponse());
+
+                logger.log(Level.INFO, "This will be a PayTrace call!");
+                break;
+
+            case SALES_CLOUD:
+//                MockIntegrationResponse oscResponse = doOSCIntegration(new MockIntegrationRequest(
+//                        IntegrationType.SalesCloud, "TEST OP WS", "READ"));
+//                logger.info(oscResponse.getResponse());
+
+                logger.log(Level.INFO, "This will be a Sales Cloud call!");
+                break;
+
+            default:
+                logger.log(Level.INFO, "Unable to route unknown message type: {0}", m.getType());
+                break;
         }
     }
 
