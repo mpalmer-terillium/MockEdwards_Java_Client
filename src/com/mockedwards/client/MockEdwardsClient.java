f7aa95d36a3e7c51373421f274becb4d052229ad
michael <mpalmer16@gmail.com>
2016-03-05 22:46:21 -0600

    cleanup and move to service based router model

--- src/com/mockedwards/client/MockEdwardsClient.java
+++ src/com/mockedwards/client/MockEdwardsClient.java
@@ -4,6 +4,7 @@ import com.mockedwards.dto.MockIntegrationRequest;
 import com.mockedwards.dto.MockIntegrationResponse;
 import com.mockedwards.factory.IntegrationFactory;
 import com.mockedwards.integration.CCIntegration;
+import com.mockedwards.integration.IntegrationType;
 import com.mockedwards.integration.OSCIntegration;
 import com.mockqueue.service.Message;
 import com.mockqueue.service.MockQueueService;
@@ -11,6 +12,15 @@ import com.mockqueue.service.MockQueueServiceService;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
+/*
+    convert this to a webapp and have it run this on startup - this way it will
+    be listening at all times to the queue.  Might not be the best solution, but
+    see if it works for now.
+
+    reference:
+    http://stackoverflow.com/questions/158336/is-there-a-way-to-run-a-method-class-only-on-tomcat-startup
+*/
 public class MockEdwardsClient {
 
     private static final Logger logger = Logger.getLogger("MockEdwardsClient");
@@ -37,19 +47,15 @@ public class MockEdwardsClient {
         switch (m.getType()) {
 
             case PAY_TRACE:
-//                MockIntegrationResponse ccResponse  = doCCIntegration(new MockIntegrationRequest(
-//                        IntegrationType.PayTrace, "100.00", "Authorization"));
-//                logger.info(ccResponse.getResponse());
-
-                logger.log(Level.INFO, "This will be a PayTrace call!");
+                MockIntegrationResponse ccResponse  = doCCIntegration(new MockIntegrationRequest(
+                        IntegrationType.PayTrace, "100.00", "Authorization"));
+                logger.info(ccResponse.getResponse());
                 break;
 
             case SALES_CLOUD:
-//                MockIntegrationResponse oscResponse = doOSCIntegration(new MockIntegrationRequest(
-//                        IntegrationType.SalesCloud, "TEST OP WS", "READ"));
-//                logger.info(oscResponse.getResponse());
-
-                logger.log(Level.INFO, "This will be a Sales Cloud call!");
+                MockIntegrationResponse oscResponse = doOSCIntegration(new MockIntegrationRequest(
+                        IntegrationType.SalesCloud, "TEST OP WS", "READ"));
+                logger.info(oscResponse.getResponse());
                 break;
 
             default:
